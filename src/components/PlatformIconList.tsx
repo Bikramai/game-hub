832576215403d630f3f84a2d132c9652c6581444
bikramia <bikram.rhce@gmail.com>
2023-12-29 13:57:27 -0500

    Displaying platform icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,45 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import _React from "react";
+import { HStack, Icon } from "@chakra-ui/react";
+import { Platform } from "../hooks/useGames";
+import { IconType } from "react-icons/lib/esm/iconBase";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: {[key: string]: IconType } = {
+    //name: PlayStation
+    //slug: playstation
+
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    android: FaAndroid,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug ]} color='green.500' />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
